a499f9992953c5824534e5e9b9fe1e1e04e7ce5c
umbranian0 <vasiletimotin@outlook.pt>
2020-01-14 12:24:14 +0000

    Inserted more some Todo's

--- newsFinderExecuter.php
+++ newsFinderExecuter.php
@@ -2,7 +2,7 @@
 //wg2.php
 require_once "NewsFinder_V2.php";
 define("FOSSBYTES_URL", "https://fossbytes.com/");
-
+const FONTES_NOTICIAS_APP =["Fossbytes" ];
 function fossbytesUrlTest(){
 
     $FossBytesNewFinder = new NewsFinder_V2("news" , FOSSBYTES_URL);
@@ -12,9 +12,10 @@ function fossbytesUrlTest(){
     $aPairs = $FossBytesNewFinder->allHyperlinksAtBoardUrl($firstPage);
 
     $results = fossBytesFilteringArray($aPairs) ;
-
+    echo("A filtrar valores!");
     //TODO -- Store values on DB
     storeValuesDb($results);
+    echo("A retornar valores");
     return $results;
 }//FossBytes
 
@@ -28,13 +29,43 @@ function fossBytesFilteringArray(array $pArrayToFilter){
 }
 
     //TODO -- Create an CMD menu
-function menu(){
+function execMenu(){
+    echo("1 -> Ver noticias atuais ". FONTES_NOTICIAS_APP ." /n
+    2 -> Pesquisar noticia por dia /n
+    3 -> Ver noticias HTML
+    4 -> sair");
+
+    $input = readline("Command: ");
     //escreve a lista de noticias
-    var_dump (fossbytesUrlTest());
-}
+    switch($input){
+        case 1 :
+            var_dump (fossbytesUrlTest());
+            break;
+        case 2:
+            $inputSubMenu = readline("Data a pesquisar: ");
+            pesquisarNoticiasDB_porDia($inputSubMenu);
+            break;
+        case 3:
+            verNoticiasHTML();
+            break;
+        case 4:
+            exit();
+            break;
+    }
+
+}//execMenu
 //TODO -- Create an CMD menu
 function storeValuesDb(array $pArrayToStore){
     //ter em atenção duplicados
 }
+//TODO --Criar uma pesquisa na DB
+function pesquisarNoticiasDB_porDia(string $pSubMenuInput){
 
-menu();
+}
+//TODO --Criar e abrir um HTML no browser
+function verNoticiasHTML(){
+
+}
+
+
+execMenu();
